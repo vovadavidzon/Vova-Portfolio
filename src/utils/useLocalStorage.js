975337783dc58f09b3885d896628e2585d21316f
vovadavidzon <vovadavidzon30@gmail.com>
2024-01-06 15:51:36 +0200

    fix bug

--- src/utils/useLocalStorage.js
+++ src/utils/useLocalStorage.js
@@ -3,7 +3,11 @@ import { useState, useEffect } from "react";
 function getStorageValue(key, defaultValue) {
   // getting stored value
   const saved = localStorage.getItem(key);
-  const initial = JSON.parse(saved);
+  let initial;
+  if (saved) {
+    initial = JSON.parse(saved);
+  }
+
   return initial || defaultValue;
 }
 
