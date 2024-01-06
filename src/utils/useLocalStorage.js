423180d769dcc038a5de194685e862896be7efbd
vovadavidzon <vovadavidzon30@gmail.com>
2024-01-06 15:56:14 +0200

    fix

--- src/utils/useLocalStorage.js
+++ src/utils/useLocalStorage.js
@@ -1,24 +1,30 @@
 import { useState, useEffect } from "react";
 
 function getStorageValue(key, defaultValue) {
-  // getting stored value
-  const saved = localStorage.getItem(key);
-  let initial;
-  if (saved) {
-    initial = JSON.parse(saved);
-  }
+  try {
+    // Getting stored value
+    const saved = localStorage.getItem(key);
 
-  return initial || defaultValue;
+    // Parse the JSON only if the stored value is not undefined
+    return saved ? JSON.parse(saved) : defaultValue;
+  } catch (error) {
+    // Handle the error (e.g., set a default value or log the error)
+    console.error("Error parsing JSON:", error);
+    return defaultValue;
+  }
 }
 
 export const useLocalStorage = (key, defaultValue) => {
-  const [value, setValue] = useState(() => {
-    return getStorageValue(key, defaultValue);
-  });
+  const [value, setValue] = useState(() => getStorageValue(key, defaultValue));
 
   useEffect(() => {
-    // storing input name
-    localStorage.setItem(key, JSON.stringify(value));
+    try {
+      // Storing input name
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      // Handle the error (e.g., log the error)
+      console.error("Error storing data in localStorage:", error);
+    }
   }, [key, value]);
 
   return [value, setValue];
